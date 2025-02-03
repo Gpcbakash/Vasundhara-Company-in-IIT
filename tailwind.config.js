aa636e6945f784b2531b9bda98194449958aee28
Akash Thakur <rajakash9720@gmail.com>
2025-02-03 20:29:10 +0530

    added react router and added some pages for testing purpose

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensures Tailwind scans your React/Vite project
+    "./components/**/*.{js,ts,jsx,tsx}" // Includes component folders
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
