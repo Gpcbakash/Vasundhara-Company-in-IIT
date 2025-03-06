dc530a3d2d17ea8bd960aca79d4008086ea4f3e0
Akash Thakur <rajakash9720@gmail.com>
2025-03-06 23:23:19 +0530

    bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";  
 import Contact from "./components/Contact";  
+import { Navbar } from "@material-tailwind/react";
 
 
 function App() {
   return (
     <>
        {/* Add Navbar here */}
+       <Navbar/>
 
       <Routes>
         <Route path="/" element={<Home />} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Ensures Tailwind scans your React/Vite project
-    "./components/**/*.{js,ts,jsx,tsx}" // Includes component folders
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
